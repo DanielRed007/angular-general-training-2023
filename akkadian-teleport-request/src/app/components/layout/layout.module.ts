cb2c597efa06c16e4f7ca311ae798ad822c90712
danielred007 <danielrojo1927@gmail.com>
2023-08-21 06:18:19 +0100

    color theme initial setup

--- akkadian-teleport-request/src/app/components/layout/layout.module.ts
+++ akkadian-teleport-request/src/app/components/layout/layout.module.ts
@@ -1,12 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AkkadianHeaderComponent } from './akkadian-header/akkadian-header.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule, 
+    MatButtonModule, 
+    MatIconModule,
+    MatGridListModule
   ],
-  declarations: [AkkadianHeaderComponent],
-  exports: [AkkadianHeaderComponent]
+  declarations: [
+    AkkadianHeaderComponent, 
+    LayoutComponent
+  ],
+  exports: [
+    AkkadianHeaderComponent, 
+    LayoutComponent
+  ]
 })
 export class LayoutModule { }
