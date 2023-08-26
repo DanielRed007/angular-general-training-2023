c3cf5937c19c12bb6c65002d51559940e07494f3
danielred007 <danielrojo1927@gmail.com>
2023-08-26 23:57:05 +0100

    changes

--- akkadian-teleport-request/src/app/app-routing.module.ts
+++ akkadian-teleport-request/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LayoutRoutingModule } from './components/layout/layout.routing.module';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    LayoutRoutingModule
+  ],
+  providers: []
 })
 export class AppRoutingModule { }
