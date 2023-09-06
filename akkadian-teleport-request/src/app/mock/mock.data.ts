e41117c2f901330d9aa535326f1e914ade1c6ef7
danielred007 <danielrojo1927@gmail.com>
2023-09-06 07:46:14 +0100

    request card list on the way

---
+++ akkadian-teleport-request/src/app/mock/mock.data.ts
@@ -0,0 +1,16 @@
+
+export const locations = [
+    {value: 'venus', viewValue: 'Venus'},
+    {value: 'mercury', viewValue: 'Mercury'},
+    {value: 'earth', viewValue: 'Earth'},
+    {value: 'mars', viewValue: 'Mars'},
+    {value: 'jupiter', viewValue: 'Jupiter'},
+    {value: 'saturn', viewValue: 'Saturn'},
+    {value: 'uranus', viewValue: 'Uranus'},
+    {value: 'pluto', viewValue: 'Pluto'},
+    {value: 'moon', viewValue: 'Moon'},
+    {value: 'europe', viewValue: 'Europe'},
+    {value: 'ganimedes', viewValue: 'Ganimedes'},
+    {value: 'enceladus', viewValue: 'Enceladus'},
+    {value: 'titan', viewValue: 'Titan'},
+];
--- akkadian-teleport-request/src/app/services/request.service.ts
+++ akkadian-teleport-request/src/app/services/request.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { PersonalInfo, Request } from "../interfaces/request";
+import { BehaviorSubject, Observable, combineLatest, map } from 'rxjs';
+import { PersonalInfo, DestinationInfo, Request } from "../interfaces/request";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
-  private personalInfo = new BehaviorSubject<PersonalInfo>({
+  private personalInfo: BehaviorSubject<PersonalInfo> = new BehaviorSubject({
     name: "",
     lastname: "",
     akkadianId: "",
@@ -14,14 +14,35 @@ export class RequestService {
     planetOrigin: ""
   });
 
+  private destinationInfo: BehaviorSubject<DestinationInfo>  = new BehaviorSubject({
+    origin: "",
+    destination: "",
+    roundTrip: "",
+    departureDate: "",
+    returnDate: ""
+  });
+
+  private requestList: BehaviorSubject<any[]> = new BehaviorSubject([{}]);
+
   _personalInfo$ = this.personalInfo.asObservable();
+  _destinationInfo$ = this.destinationInfo.asObservable();
+  _resquestList$ = this.requestList.asObservable();
 
   getPersonalInfo(): Observable<PersonalInfo>{
     return this._personalInfo$;
   }
 
+  getDestinationInfo(): Observable<DestinationInfo>{
+    return this._destinationInfo$;
+  }
+
   setPersonalInfo(latestValue: PersonalInfo){
-    console.log(latestValue, "val");
+    console.log(latestValue, "personal");
     return this.personalInfo.next(latestValue);
+  }
+
+  setDestinationInfo(latestValue: DestinationInfo){
+    console.log(latestValue, "destination");
+    return this.destinationInfo.next(latestValue);
   }
 }
